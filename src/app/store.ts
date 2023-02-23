4d81ea33bff6b95e1a1b5766fd26a4eb76b829a4
naftali rayniz <naftali@wikey.ai>
2023-02-23 09:56:59 +0200

    rm unused files

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-// import { setupListeners } from '@reduxjs/toolkit/dist/query/core/setupListeners';
-import counterReducer from '../features/counter/counterSlice';
 import monitorReducer from '../pages/Monitor/state/monitorSlice';
 import { api } from '../pages/Monitor/state/websocket';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     monitor: monitorReducer,
     [api.reducerPath]: api.reducer,
   },
@@ -14,8 +11,6 @@ export const store = configureStore({
     serializableCheck: false
   }).concat([api.middleware]),
 });
-
-// setupListeners(store.dispatch)
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
