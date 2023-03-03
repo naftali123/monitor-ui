3ea9b2ed8b4f456de3694239b554dc3a3e601b2e
naftali rayniz <naftali@wikey.ai>
2023-03-03 13:47:02 +0200

    text & date utils

--- src/pages/components/utils.ts
+++ src/pages/components/utils.ts
@@ -1,2 +1,7 @@
-
 export const upFirstLetter = (text: string) => text.split("").map((e, i) => i === 0 ? e.toUpperCase() : e).join("");
+
+export const separateCamelCase = (text: string) => text.split(/(?=[A-Z])/).join(" ");
+
+export const separateCamelCaseAndUpFirstLetter = (text: string) => upFirstLetter(separateCamelCase(text).toLowerCase());
+
+export const isValidDate = (date: string): boolean => new Date(date).toString() !== 'Invalid Date';
