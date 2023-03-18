e6e2afa85b40c8b4ef3ec6149f630ce9c62dcea3
naftali rayniz <naftali@wikey.ai>
2023-03-18 22:45:38 +0200

    renaming api path

--- src/pages/SignUp/state/config/api.ts
+++ src/pages/SignUp/state/config/api.ts
@@ -4,8 +4,8 @@ export const API = {
     ...GLOBAL_API,
     USER: {
         SIGN_UP: '/users/sign-up',
-        LOG_IN: '/users/login',
-        SIGN_OUT: '/users/sign-out',
+        LOGIN: '/users/login',
+        LOGOUT: '/users/sign-out',
         GET_USER_BY_ID: '/users/:id',
         GET_USER_BY_EMAIL: '/users/email/:email',
     }
