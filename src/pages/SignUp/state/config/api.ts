70ff7f14ea268810dd83c2ecd17495a456c86776
naftali rayniz <naftali@wikey.ai>
2023-03-19 02:48:14 +0200

    update routes path

--- src/pages/SignUp/state/config/api.ts
+++ src/pages/SignUp/state/config/api.ts
@@ -3,9 +3,10 @@ import { API as GLOBAL_API } from '../../../../config';
 export const API = {
     ...GLOBAL_API,
     USER: {
-        SIGN_UP: '/users/sign-up',
-        LOGIN: '/users/login',
-        LOGOUT: '/users/sign-out',
+        LOCAL_SIGNUP: '/users/local/signup',
+        LOCAL_LOGIN: '/users/local/login',
+        LOGOUT: '/users/logout',
+        REFRESH_TOKEN: '/users/refresh',
         GET_USER_BY_ID: '/users/:id',
         GET_USER_BY_EMAIL: '/users/email/:email',
     }
