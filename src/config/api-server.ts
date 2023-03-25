099e9720fd02085be05a655234bab493f84a0049
naftali rayniz <naftali@wikey.ai>
2023-03-25 23:28:03 +0300

    cors configurations for api server

--- src/config/api-server.ts
+++ src/config/api-server.ts
@@ -1,5 +1,7 @@
 export const HOST = 'localhost';
-export const DEFAULT_PORT = 8080;
+export const DEFAULT_PORT = 3000;//8080;
+export const API_URL = '';
 export const HEADERS = {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
 };
